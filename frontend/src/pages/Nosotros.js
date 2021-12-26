643109a413fc2b4bb7cca4a8a803daf30c190c29
PedroSalduna <pewemes@gmail.com>
2021-12-26 15:53:30 -0300

    ico

--- frontend/src/pages/Nosotros.js
+++ frontend/src/pages/Nosotros.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/NavBar'
 import Footer from '../components/Footer'
+import Separator from '../components/Separator'
 
 
 const Nosotros = () => {
@@ -8,6 +9,7 @@ const Nosotros = () => {
         return(
             <>
                 <Navbar/>
+                <Separator/>
                 <Footer/>
             </>
         )
