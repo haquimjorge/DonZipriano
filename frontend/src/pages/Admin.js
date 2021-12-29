4c90f90b9e9c017fdfd05864729842b6206d82bd
Kevhausen <kevin.vonhausen@gmail.com>
2021-12-29 19:19:34 -0300

    Se agrega menu de admin panel en alfa

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -15,10 +15,13 @@ import { dividerClasses } from "@mui/material";
 import Pencil from '../assets/pencil.png'
 import Tooltip from "react-bootstrap/Tooltip";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import AdminTabPanel from "../components/AdminTabPanel";
 
 const Admin = (props) => {
     const [key, setKey] = useState('menu');
     const [selectedId, setSelectedId] = useState('')
+    const [editType, setEditType] = useState("");
+    const [editInput, setEditInput] = useState('')
     const [edit, setEdit] = useState(false)
 
     useEffect(()=>{
@@ -30,23 +33,67 @@ const Admin = (props) => {
         </Tooltip>
       );
 
-      function handleEdit(id, type){
-          if(selectedId === id){
-              setSelectedId('')
-          }else if(selectedId === "" ){
-              setSelectedId(id)
-          }else if(selectedId !== "" && selectedId !==id){
-            setSelectedId(id)
+      function handleEdit(id, input, type){
+          switch(type){
+            case "name":
+                console.log("paso por el name")
+                setEditType(type)
+                setEditInput(input)
+                if(selectedId === id && editType === type){
+                    setSelectedId('')
+                    console.log(1)
+                }else if(selectedId === "" ){
+                    setSelectedId(id)
+                    console.log(2)
+                }else if(selectedId !== "" && selectedId !==id){
+                  setSelectedId(id)
+
+                  console.log(3)
+                }
+                break
+            case 'description':
+                console.log("paso por el description")
+                setEditType(type)
+                setEditInput(input)
+                if(selectedId === id && editType === type){
+                    setSelectedId('')
+                }else if(selectedId === "" ){
+                    setSelectedId(id)
+                    
+                }else if(selectedId !== "" && selectedId !==id){
+                  setSelectedId(id)
+
+                }
+                break
+            
+            case 'price':
+                console.log("paso por el price")
+                setEditType(type)
+                setEditInput(input)
+                if(selectedId === id && editType === type){
+                    setSelectedId('')
+                }else if(selectedId === "" ){
+                    setSelectedId(id)
+                }else if(selectedId !== "" && selectedId !==id){
+                  setSelectedId(id)
+                }
+                break
+            default:
+                return{
+
+                      }
           }
+
+          
         //  aqui capturo el id de la comida y si es precio o no
       }
 
-    let editPencil = <OverlayTrigger
+    let editPencil=(plate,type) => <OverlayTrigger
     placement="top"
     delay={{ show: 25, hide: 25 }}
     overlay={renderEdit}
   >
-    <img onClick={()=>handleEdit()} src={Pencil} alt='pencil' className="admin-edit-icon"/>
+    <img onClick={()=>handleEdit(plate._id,plate[type], type)} src={Pencil} alt='pencil' className="admin-edit-icon"/>
     </OverlayTrigger>
 
 
@@ -56,10 +103,6 @@ const Admin = (props) => {
     let deserts =props.meals.filter(meal=> meal.timeFood === "Postre")
     let drinks =props.meals.filter(meal=> meal.timeFood === "Bebestible")
 
-    console.log('COMPONENT: ESTO ME LLEGA DE PROPS.MEALS')
-    console.log(props.meals)
-    console.log('COMPONENT: ESTA ES ENTRADA')
-    console.log(entryPlates)
   return (
     <>
       <NavBar />
@@ -84,39 +127,46 @@ const Admin = (props) => {
     <Col sm={3}>
       <Nav variant="pills" className="flex-column">
         <Nav.Item>
-          <Nav.Link eventKey="first">entradas</Nav.Link>
+          <Nav.Link eventKey="entryPlates">entradas</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="second">platos principales</Nav.Link>
+          <Nav.Link eventKey="mainPlates">platos principales</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="third">postres</Nav.Link>
+          <Nav.Link eventKey="deserts">postres</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="fourth">bebestibles</Nav.Link>
+          <Nav.Link eventKey="drinks">bebestibles</Nav.Link>
         </Nav.Item>
       </Nav>
     </Col>
     <Col sm={9}>
       <Tab.Content>
-        <Tab.Pane eventKey="first" className="bg-dark">
+          {/* <AdminTabPanel timeFood="entryPlates" meals={entryPlates} /> */}
+         <Tab.Pane eventKey="entryPlates">
           <p>entradas</p>
           <div className="d-flex flex-wrap">
           {entryPlates.map(plate=> 
            <Card className="col-12 col-md-6 col-xxl-4 col-xl-4 col-lg-6 col-sm-12 col-xs-12">
            <Card.Img variant="top" src={plate.image} />
-           <Card.Body>
+           <Card.Body className="admin-card-body d-flex flex-column align-items-around justify-content-between">
                <Row>
-                   <Col xs={1} sm={1} lg={1} md={1}  className="p-0">{editPencil}</Col>
-                   <Col xs={11} sm={11} lg={11} md={11} className="p-0"><Card.Title className="text-dark">{plate.name}</Card.Title></Col>
+                   <Col xs={1} sm={1} lg={1} md={1}  className="p-0">{editPencil(plate,'name')}</Col>
+                   <Col xs={11} sm={11} lg={11} md={11} className="p-0">{selectedId === plate._id && editType=== 'name'?  <OverlayTrigger
+                              key="1"
+                              placement="top"
+                              overlay={
+                                <Tooltip id="key">Presiona enter para editar</Tooltip>
+                              }
+                            ><input value={editInput} onChange={(e) => setEditInput(e.target.value)} /></OverlayTrigger> : <Card.Text className="text-dark">{plate.name}</Card.Text>}</Col>
                </Row>
                <Row>
-                   <Col xs={1} sm={1} lg={1} md={1}  className="p-0">{editPencil}</Col>
-                   <Col xs={11} sm={11} lg={11} md={11} className="p-0">{edit? <input value='estoy ditando ctm'></input> : <Card.Text className="text-dark">{plate.description}</Card.Text>}</Col>
+                   <Col xs={1} sm={1} lg={1} md={1}  className="p-0">{editPencil(plate,'description')}</Col>
+                   <Col xs={11} sm={11} lg={11} md={11} className="p-0">{selectedId === plate._id && editType=== 'description'?  <textarea className="admin-textarea" rows="3" value={editInput} onChange={(e) => setEditInput(e.target.value)}></textarea> : <Card.Text className="text-dark">{plate.description}</Card.Text>}</Col>
                </Row>
                <Row>
-                   <Col xs={1} sm={1} lg={1} md={1}  className="p-0">{editPencil}</Col>
-                   <Col xs={11} sm={11} lg={11} md={11} className="p-0"><Card.Text className="text-dark">{plate.price}</Card.Text></Col>
+                   <Col xs={1} sm={1} lg={1} md={1}  className="p-0">{editPencil(plate,'price')}</Col>
+                   <Col xs={11} sm={11} lg={11} md={11} className="p-0">{selectedId === plate._id && editType=== 'price'?  <input value={editInput} onChange={(e) => setEditInput(e.target.value)}></input> : <Card.Text className="text-dark">${plate.price}</Card.Text>}</Col>
                </Row> 
            </Card.Body>
            <Card.Footer className="bg-danger">
@@ -125,8 +175,8 @@ const Admin = (props) => {
          </Card>  
             )}
             </div>
-        </Tab.Pane>
-        <Tab.Pane eventKey="second">
+        </Tab.Pane> 
+        <Tab.Pane eventKey="mainPlates">
         <p>platos principales</p>
         <div className="d-flex flex-wrap">
           {mainPlates.map(plate=> 
@@ -148,7 +198,7 @@ const Admin = (props) => {
             )}
             </div>
         </Tab.Pane>
-        <Tab.Pane eventKey='third'>
+        <Tab.Pane eventKey='deserts'>
             <p>postres</p>
             <div className="d-flex flex-wrap">
           {deserts.map(plate=> 
@@ -170,7 +220,7 @@ const Admin = (props) => {
             )}
             </div>
         </Tab.Pane>
-        <Tab.Pane eventKey="fourth">
+        <Tab.Pane eventKey="drinks">
             <p>bebestibles</p>
             <div className="d-flex flex-wrap">
           {drinks.map(plate=> 
