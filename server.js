83d3aecb13c467c824b21ae627d3589edc8fb51d
haquimjorge <haquimjp@gmail.com>
2021-12-30 03:49:06 -0300

    heroku

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ require("./config/database.js");
 const passport = require("passport");
 const app = express();
 
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
 
 app.use("/api", Router);
 
-app.listen(process.env.PORT || 4000, process.env.HOST || "0.0.0.0", () => {
-  console.log(` El server esta en el puerto ${process.env.PORT || 4000}`);
+app.listen(process.env.PORT || "5000", () => {
+  console.log(`El server esta en el puerto ${process.env.PORT || "5000"}`);
 });
