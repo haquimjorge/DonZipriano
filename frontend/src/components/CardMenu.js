69d29fcd61505b31503f97e54137dd6d84cbda86
haquimjorge <haquimjp@gmail.com>
2021-12-30 02:29:07 -0300

    Agrego estilos y responsividad nueva a seperatos

--- frontend/src/components/CardMenu.js
+++ frontend/src/components/CardMenu.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Slider from "@mui/material/Slider";
-import Likes from "./Likes"
+import Likes from "./Likes";
 import "animate.css";
 
 const CardMenu = ({ meal }) => {
@@ -20,29 +20,11 @@ const CardMenu = ({ meal }) => {
     setValue(newValue);
   };
 
-
   return (
     <>
       <div className="card-menu-container">
         <div className="card-menu animate__animated animate__fadeInLeft">
-          {/* <Slider
-              value={value}
-              onChange={handleChange}
-              aria-labelledby="continuos-slider"
-              min={200}
-              max={1000}
-              valueLabelDisplay="auto"
-              color="secondary"
-            /> */}
-
           <div className="title-menu-container">
-            {/* <div className="logo-menu">
-              <img
-                id="logo-nav"
-                src="/assets/DonZLogo.png"
-                alt="Logo Don Zipriano"
-              />
-            </div> */}
             <div className="titulo-menu">
               <h1>Menu </h1>
             </div>
@@ -58,7 +40,7 @@ const CardMenu = ({ meal }) => {
                   min={650}
                   max={1000}
                   valueLabelDisplay="auto"
-                  color="secondary"
+                  sx={{ color: "#b2102f" }}
                 />
               </div>
             </div>
@@ -74,18 +56,16 @@ const CardMenu = ({ meal }) => {
                           <p>{entrada.description}</p>
                           <p>{entrada.type}</p>
                           <p className="precios-menu">$ {entrada.price}</p>
-                         
                         </div>
                         <div className="divlikes">
-                        <Likes className="like" meal={entrada}/>
+                          <Likes className="like" meal={entrada} />
                         </div>
                         <div
                           className="card-food-pic"
                           style={{
                             backgroundImage: `URL(${entrada.image})`,
-                          }}>
-                  
-                          </div>
+                          }}
+                        ></div>
                       </div>
                     </div>
                   );
@@ -95,17 +75,7 @@ const CardMenu = ({ meal }) => {
           <div className="plato-principal">
             <h4>Platos Principales</h4>
             <div className="slider-container-main">
-              <div className="slider-container">
-                <Slider
-                  value={value}
-                  onChange={handleChange}
-                  aria-labelledby="continuos-slider"
-                  min={410}
-                  max={1000}
-                  valueLabelDisplay="auto"
-                  color="secondary"
-                />
-              </div>
+              <div className="slider-container"></div>
             </div>
             <div className="platos-entradas">
               {mealPlatosPrincipales
@@ -119,10 +89,9 @@ const CardMenu = ({ meal }) => {
                           <p>{plato.description}</p>
                           <p>{plato.type}</p>
                           <p className="precios-menu">$ {plato.price}</p>
-                        
                         </div>
                         <div className="divlikes">
-                        <Likes className="like" meal={plato}/>
+                          <Likes className="like" meal={plato} />
                         </div>
                         <div
                           className="card-food-pic"
@@ -137,20 +106,9 @@ const CardMenu = ({ meal }) => {
             </div>
           </div>
           <div className="postre">
-            
             <h4 className="title-postres">Postres</h4>
             <div className="slider-container-main">
-              <div className="slider-container">
-                <Slider
-                  value={value}
-                  onChange={handleChange}
-                  aria-labelledby="continuos-slider"
-                  min={450}
-                  max={1000}
-                  valueLabelDisplay="auto"
-                  color="secondary"
-                />
-              </div>
+              <div className="slider-container"></div>
             </div>
             <div className="platos-entradas">
               {mealPostres
@@ -164,10 +122,9 @@ const CardMenu = ({ meal }) => {
                           <p>{postre.description}</p>
                           <p>{postre.type}</p>
                           <p className="precios-menu">$ {postre.price}</p>
-                         
                         </div>
                         <div className="divlikes">
-                        <Likes className="like" meal={postre}/>
+                          <Likes className="like" meal={postre} />
                         </div>
                         <div
                           className="card-food-pic"
@@ -184,17 +141,7 @@ const CardMenu = ({ meal }) => {
           <div className="bebestible">
             <h4>Bebidas</h4>
             <div className="slider-container-main">
-              <div className="slider-container">
-                <Slider
-                  value={value}
-                  onChange={handleChange}
-                  aria-labelledby="continuos-slider"
-                  min={200}
-                  max={1000}
-                  valueLabelDisplay="auto"
-                  color="secondary"
-                />
-              </div>
+              <div className="slider-container"></div>
             </div>
             <div className="platos-entradas">
               {mealBebidas
@@ -208,10 +155,9 @@ const CardMenu = ({ meal }) => {
                           <p>{bebida.description}</p>
                           <p>{bebida.type}</p>
                           <p className="precios-menu">$ {bebida.price}</p>
-                         
                         </div>
                         <div className="divlikes">
-                        <Likes className="like" meal={bebida}/>
+                          <Likes className="like" meal={bebida} />
                         </div>
                         <div
                           className="card-food-pic"
